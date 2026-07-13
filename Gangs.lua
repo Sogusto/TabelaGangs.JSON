89feb839d518b5701c2450a158ddb75671fce576
Sogusto <sogusto89731@hotmail.com>
2026-07-13 20:19:51 -0300

    Refactor Gangs.lua to return gang data structure
    Refactored Gangs.lua to return a table of gangs instead of defining it directly.

--- Gangs.lua
+++ Gangs.lua
@@ -1,14 +1,14 @@
-local Gangs = {
-	{
-		Grupo = 34782392,
-		MinRank = 15,
-		GangNome = "Os Hashiras",
-		MaxHealth = 1000,
-		Nome = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 255) },
-		Patente = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(0, 0, 127) },
-		Team = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(0, 0, 127) },
-		Items = {"RinneganPushActivator","Mundo","Kunai","SocoEletrico","PISOLUASEVENTO","dismantelar","CollateralRuin","soco","SusanooT"},
-	},
+return {
+    {
+        Grupo = 34782392,
+        MinRank = 15,
+        GangNome = "Os Hashiras",
+        MaxHealth = 1000,
+        Nome = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 255) },
+        Patente = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(0, 0, 127) },
+        Team = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(0, 0, 127) },
+        Items = {"RinneganPushActivator","Mundo","Kunai","SocoEletrico","PISOLUASEVENTO","dismantelar","CollateralRuin","soco","SusanooT"},
+    },
+    -- coloque mais gangues aqui
 }
-
 -- coloque mais gangues aqui
