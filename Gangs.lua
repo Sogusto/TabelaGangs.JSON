2857650b6215865aedba96861cc4e36742b21d40
Sogusto <sogusto89731@hotmail.com>
2026-07-13 20:39:52 -0300

    Update gang details and add new gang entry

--- Gangs.lua
+++ Gangs.lua
@@ -2,13 +2,22 @@ return {
     {
         Grupo = 34782392,
         MinRank = 15,
-        GangNome = "Os",
+        GangNome = "Tokyo Manji Gang",
         MaxHealth = 1000,
-        Nome = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 25) },
-        Patente = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(0, 0, 127) },
-        Team = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(0, 0, 127) },
-        Items = {"RinneganPushActivator","Mundo","Kunai","SocoEletrico","PISOLUASEVENTO","dismantelar","CollateralRuin","soco","SusanooT"},
+        Nome = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 255) },
+        Patente = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 255) },
+        Team = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 25) },
+        Items = {"DoubleJump","Mundo","Kunai","SocoEletrico","PISOLUASEVENTO","dismantelar","CollateralRuin","soco","SusanooT"},
     },
-    -- coloque mais gangues aqui
-}
+  	{
+		Grupo = 35831920,
+		MinRank = 5,
+		GangNome = "Caçadores De Onis",
+		MaxHealth = 1000,
+		Nome = { Letraakakakaka = Color3.fromRGB(255, 170, 0), Borda = Color3.fromRGB(0, 0, 0) },
+		Patente = { Letraakakakaka = Color3.fromRGB(255, 170, 0), Borda = Color3.fromRGB(0, 0, 0) },
+		Team = { Letraakakakaka = Color3.fromRGB(255, 170, 0), Borda = Color3.fromRGB(255, 170, 0) },
+		Items = {"Susanoo","Raio","Rinnegan","Kokusen","DoubleJump","Kunai","CollateralRuin","Mundo","CorteCircular"},
+	},
+        
 -- coloque mais gangues aqui
