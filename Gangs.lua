e8568c896c3143b4257fad769c073e64eca18e7e
Sogusto <sogusto89731@hotmail.com>
2026-07-13 22:24:30 -0300

    Add new gang 'Blue Lock' with attributes

--- Gangs.lua
+++ Gangs.lua
@@ -19,5 +19,14 @@ return {
 		Team = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(227, 196, 255) },
 		Items = {"Susanoo","Raio","Rinnegan","Kokusen","DoubleJump","Kunai","CollateralRuin","Mundo","CorteCircular"},
 	},
+    {
+		Grupo = 35371915,
+		MinRank = 2,
+		GangNome = "Blue Lock",
+		MaxHealth = 50	00,
+		Nome = { Letraakakakaka = Color3.fromRGB(107, 50, 124), Borda = Color3.fromRGB (196, 112, 160) },
+		Patente = { Letraakakakaka = Color3.fromRGB(125, 187, 221), Borda = Color3.fromRGB(255, 255, 255) },
+		Team = { Letraakakakaka = Color3.fromRGB(123, 46, 47), Borda = Color3.fromRGB(0, 0, 0) },
+		Items = {"Susanoo","Raio","Rinnegan","Kokusen","DoubleJump","Kunai","CollateralRuin","Mundo","CorteCircular"},
 }     
 -- coloque mais gangues aqui
