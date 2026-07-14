988648b7343c57651d2c4dc5a4ebf926f6a97236
pedrosalesdcarvalho-png <pedrosalesdcarvalho@gmail.com>
2026-07-14 19:00:11 -0300

    Atualizar o Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -39,7 +39,7 @@ return {
         Team = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(139, 0, 0) },
         Items = {"DoubleJump","Mundo","Kunai","SocoEletrico","pisao","dismantelar","CollateralRuin","soco","SusanooT","BlackHole"},
     },
-  	{
+{
 }     
 }
 -- coloque mais gangues aqui
