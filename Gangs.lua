1e9150a793a9e05cff3e0040c8ea29a88146e8be
Sogusto <sogusto89731@hotmail.com>
2026-07-23 15:48:19 -0300

    Add 'Hino' to the Items list in Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -7,7 +7,7 @@ return {
         Nome = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 255) },
         Patente = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 255) },
         Team = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 25) },
-        Items = {"PisoVento","DoubleJump","Mundo","Kunai","SocoEletrico","pisao","IceParadise","CollateralRuin","soco","SusanooT"},
+        Items = {"PisoVento","DoubleJump","Mundo","Kunai","SocoEletrico","pisao","IceParadise","CollateralRuin","soco","SusanooT","Hino"},
     },
   	{
 		Grupo = 33345163,
