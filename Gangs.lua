e9693e600b71f6db7e1bf2fe005cf45d4d7cc1cb
pedrosalesdcarvalho-png <pedrosalesdcarvalho@gmail.com>
2026-07-31 00:29:01 -0300

    Atualizar o Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -37,7 +37,7 @@ return {
         Nome = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 255) },
         Patente = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 255) },
         Team = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(128, 0, 0) },
-        Items = {"DoubleJump","PisoVento","Kunai","SocoEletrico","","Hino","CollateralRuin","IceParadise","SusanooT","","CorteCircular","Rinnegan",""},
+        Items = {"DoubleJump","PisoVento","Kunai","SocoEletrico","Chute Voador","Hino","CollateralRuin","IceParadise","SusanooT","","CorteCircular","Rinnegan",""},
 },
 {
     Grupo = 916548506,
