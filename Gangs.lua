8c4c53e81afd7a3078fc3338c4500882b49d82c1
Ryvenxz <pedrosalesdcarvalho@gmail.com>
2026-08-02 19:28:52 -0300

    Atualizar o Gangs.lua

--- Gangs.lua
+++ Gangs.lua
@@ -43,7 +43,10 @@ return {
         Patente = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(255, 255, 255) },
         Team = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(128, 0, 0) },
         Items = {"DoubleJump","PisoVento","Kunai","SocoEletrico","ChuteVoador","Hino","CollateralRuin","IceParadise","SusanooG","CorteCircular","Rinnegan","DashCorvo"},
-},
+},		
+  adicionais = {
+        Shirt = 7602845081,
+        Pants = 108311724406011,
 {
     Grupo = 916548506,
 	MinRank = 1,
