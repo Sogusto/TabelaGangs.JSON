3958bfd220e68fdd43b408406af7ab5e9138c360
Sogusto <sogusto89731@hotmail.com>
2026-08-02 20:30:14 -0300

    Remove adicionais section from Gangs.lua
    Removed adicionais section from Gangs.lua.

--- Gangs.lua
+++ Gangs.lua
@@ -45,10 +45,6 @@ return {
         Team = { Letraakakakaka = Color3.fromRGB(0, 0, 0), Borda = Color3.fromRGB(128, 0, 0) },
         Items = {"DoubleJump","PisoVento","Kunai","SocoEletrico","ChuteVoador","Hino","CollateralRuin","IceParadise","SusanooT","CorteCircular","Rinnegan","DashCorvo"},
 	},
-				adicionais = {
-        Shirt = 7602845081,
-        Pants = 108311724406011,
-}
 },		
  {
     Grupo = 916548506,
